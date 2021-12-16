769afcdda1510f011b4c7ee17649c029ea1af807
Andrew Ha <93740156+Andrew-JG-Ha@users.noreply.github.com>
2021-12-15 23:02:32 -0800

    Update WifiModuleA.ino
    Added guards and error checks, more robust connection between two modules

--- WifiModuleA.ino
+++ WifiModuleA.ino
@@ -2,6 +2,23 @@
 #include <math.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+//VARIABLES FOR DHT11 SENSOR (temperature and humidity sensor)
+#define DHTTYPE DHT11     // DHT 11
+#define DHTPIN 5 //D1
+  unsigned long timer_DHT11 = 0;
+  unsigned long timerStart_DHT11 = 0;
+  unsigned long readDelay = 30000;
+  float temperatureValue = 0;
+  float humidityValue = 0;
+  sensors_event_t event;
+  int state_DHT11 = 0;
+  int statePrevious_DHT11 = 0;  
+DHT_Unified dht(DHTPIN, DHTTYPE);
+  int LastDHTState = 0;
 
 //VARIABLES FOR REGULATORS: (Patterns)
 const int toRegulatorA = 14; //D5
@@ -16,19 +33,24 @@ const int toRegulatorD = 15; //D8
   int statePrevious_PatternC = 0;
   int state_PatternD = 0;
   int statePrevious_PatternD = 0;
+  int state_PatternDHT = 0;
+  int statePrevious_PatternDHT = 0;
   unsigned long timer_pattern = 0;
   unsigned long timerStart_pattern = 0;
 
 //CONTROL VARIABLES:
+
 const bool DEBUG = true;
-  unsigned long inputDelayTime;
-  int inputPattern;
+
+  unsigned long inputDelayTime = 5000;
+  int inputPattern = 0;
   bool inputIsActive;
   bool inputIsSync;
   int count = 0;
 const int loopPatternA = 3;
 const int loopPatternB = 2;
 const int loopPatternC[2] = {2,3};
+const int loopPatternDHT[4] = {1,2,3,4};
 const int numberPatterns = 4; //number of patterns that currently exist
 
 //STRUCTURE TO BE SENT OUT:
@@ -43,10 +65,14 @@ typedef struct moduleBData { //module within home
   int pattern;
   bool isActive;
   bool isSync;
+  int statePatternA = 0;
+  int statePatternB = 0;
+  int statePatternC = 0;
+  int statePatternD = 0;
 } moduleBData;
 
 //VARIABLES DEALING WITH WIFI:
-uint8_t broadcastAddress[] = {0x40,0xF5,0x20,0x3E,0x28,0x35}; //MAC of wifi module B
+uint8_t broadcastAddress[] = {0x40, 0xF5, 0x20, 0x3E, 0x28, 0x35}; //MAC of wifi module B
   moduleAData outputData; //package to be shipped to module B
   moduleBData inputData;
 
@@ -55,7 +81,7 @@ void setup() {
   pinMode(toRegulatorB, OUTPUT);
   pinMode(toRegulatorC, OUTPUT);
   pinMode(toRegulatorD, OUTPUT);
-  
+  dht.begin();  
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   if (DEBUG == true) {
@@ -74,10 +100,8 @@ void setup() {
 void loop() {
   if (inputIsActive == true) {
     if (inputIsSync == true) {
+      state_PatternDHT = 0;
       if (inputPattern == 1){
-        if (state_PatternA == 1){ //is in wait state, run it
-          state_PatternA == 2;
-        }
         FSM_patternA();        
         if(DEBUG == true){
           if(state_PatternA != statePrevious_PatternA){
@@ -87,9 +111,6 @@ void loop() {
         }
       }
       else if (inputPattern == 2){
-        if (state_PatternB == 1){ //is in wait state, run it
-          state_PatternB == 2;
-        }
         FSM_patternB();        
         if(DEBUG == true){
           if(state_PatternB != statePrevious_PatternB){
@@ -99,9 +120,6 @@ void loop() {
         }        
       }
       else if (inputPattern == 3){
-        if (state_PatternC == 1){ //is in wait state, run it
-          state_PatternC == 2;
-        }
         FSM_patternC();        
         if(DEBUG == true){
           if(state_PatternC != statePrevious_PatternC){
@@ -111,9 +129,6 @@ void loop() {
         }         
       }
       else if (inputPattern == 4){
-        if (state_PatternD == 1){ //is in wait state, run it
-          state_PatternD == 2;
-        }
         FSM_patternD();        
         if(DEBUG == true){
           if(state_PatternD != statePrevious_PatternD){
@@ -128,6 +143,19 @@ void loop() {
     }
     else { //isSync == false, not synced together
       //get data from temp/humidity sensor and decide what to do from there
+      inputPattern = -1;
+      FSM_DHT11();
+      updateDelayTimer();
+      if (state_PatternDHT == 1){
+        state_PatternDHT = 2;
+      }
+      FSM_patternDHT();
+      if(DEBUG == true){
+        if(state_PatternDHT != statePrevious_PatternDHT){
+          Serial.print("State of DHT: ");
+          Serial.println(state_PatternDHT);
+        }
+      }        
     }
   }
   else {
@@ -139,10 +167,61 @@ void loop() {
       state_PatternB = 0;
       state_PatternC = 0;
       state_PatternD = 0;
+      state_PatternDHT = 0;
       count = 0; 
   }
 }
 
+void readDHT11(){
+  dht.temperature().getEvent(&event);
+  temperatureValue = event.temperature;
+  dht.humidity().getEvent(&event);
+  humidityValue = event.relative_humidity;
+  if (DEBUG == true) {
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      Serial.print(F("Temperature: "));
+      Serial.print(temperatureValue);
+      Serial.println(F("°C"));
+    }
+      if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      Serial.print(humidityValue);
+      Serial.println(F("%"));
+    }
+  }
+}
+
+void FSM_DHT11(){
+  statePrevious_DHT11 = state_DHT11;
+  switch(state_DHT11){
+    case(0): //RESET
+      state_DHT11 = 1;
+    break;
+    case(1): //ARM
+      timerStart_DHT11 = millis();
+      state_DHT11 = 2;
+    break;
+    case(2): //START
+      timer_DHT11 = millis();
+      if (timer_DHT11 - timerStart_DHT11 > readDelay) {
+        state_DHT11 = 3;
+      }
+    break;
+    case(3): //READ
+      readDHT11();
+      state_DHT11 = 4;
+    break;
+    case(4): //DONE
+      state_DHT11 = 0;
+    break;
+  }
+}
 
 void FSM_default(){
     digitalWrite(toRegulatorA, HIGH);
@@ -457,7 +536,173 @@ void FSM_patternC(){
 }
 
 void FSM_patternD(){
-  
+    unsigned long temp_inputDelayTime = inputDelayTime;
+    inputDelayTime = inputDelayTime / 3;
+    statePrevious_PatternD = state_PatternD;
+    switch(state_PatternD){
+      case(0): //RESET
+        count = 0;
+        state_PatternD = 1;
+      break;
+      case(1): //WAIT
+        //Do nothing, let main program initiate
+      case(2): //ITERATION 1 (Setup)
+        state_PatternD = 3;
+        timerStart_pattern = millis();
+      break;
+      case(3): //ITERATION 2
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, LOW);
+        digitalWrite(toRegulatorC, LOW);
+        digitalWrite(toRegulatorD, LOW);
+        timer_pattern = millis();
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternD = 4;
+          timerStart_pattern = millis();
+        }
+      break;
+      case(4): //ITERATION 3
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, HIGH);
+        digitalWrite(toRegulatorC, HIGH);
+        digitalWrite(toRegulatorD, HIGH);
+        timer_pattern = millis();
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternD = 5;
+          timerStart_pattern = millis();
+          }
+      break;
+      case(5): //DONE
+        if (inputPattern == 4) {
+          state_PatternD = 2;
+        }
+        else {
+          state_PatternD = 0; 
+        }
+      break;
+    }
+    inputDelayTime = temp_inputDelayTime;
+}
+
+void FSM_patternDHT(){
+    statePrevious_PatternDHT = state_PatternDHT;
+     
+    switch(state_PatternDHT){
+      case(0): //RESET
+        count = 0;
+        state_PatternDHT = 1;
+      break;
+      case(1): //WAIT
+        //Do nothing, let main program initiate
+      case(2): //ITERATION 1 (Setup)
+        state_PatternDHT = 3;
+        timerStart_pattern = millis();
+      break;
+      case(3): //ITERATION 2
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, LOW);
+        digitalWrite(toRegulatorC, LOW);
+        digitalWrite(toRegulatorD, LOW);
+        LastDHTState = 3;
+        timer_pattern = millis();
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternDHT = 7;
+          timerStart_pattern = millis();
+        }        
+      break;
+      case(4): //ITERATION 3
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, HIGH);
+        digitalWrite(toRegulatorC, LOW);
+        digitalWrite(toRegulatorD, LOW);
+        timer_pattern = millis();
+        LastDHTState = 4;
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternDHT = 7;
+          timerStart_pattern = millis();
+          }
+      break;
+      case(5): //ITERATION 4
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, LOW);
+        digitalWrite(toRegulatorC, HIGH);
+        digitalWrite(toRegulatorD, LOW);
+        timer_pattern = millis();
+        LastDHTState = 5;
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternDHT = 7;
+          timerStart_pattern = millis();
+          }
+      break;
+      case(6): //ITERATION 5
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, LOW);
+        digitalWrite(toRegulatorC, LOW);
+        digitalWrite(toRegulatorD, HIGH);
+        timer_pattern = millis();
+        LastDHTState = 6;
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternDHT = 7;
+          timerStart_pattern = millis();
+        }
+      break;
+      case(7): //ITERATION 6
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, LOW);
+        digitalWrite(toRegulatorC, LOW);
+        digitalWrite(toRegulatorD, LOW);
+        timer_pattern = millis();
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          state_PatternDHT = 8;
+          timerStart_pattern = millis();
+          }
+      break;
+      case(8): //ITERATION 7
+        digitalWrite(toRegulatorA, HIGH);
+        digitalWrite(toRegulatorB, HIGH);
+        digitalWrite(toRegulatorC, HIGH);
+        digitalWrite(toRegulatorD, HIGH);
+        timer_pattern = millis();
+        if (timer_pattern - timerStart_pattern > inputDelayTime) {
+          count++;
+          state_PatternDHT = 7;
+          timerStart_pattern = millis();
+          }
+        if (count >= loopPatternDHT[0] && LastDHTState == 3) {
+          state_PatternDHT = 4;
+          count = 0;
+        }
+        else if (count >= loopPatternDHT[1] && LastDHTState == 4) {
+          state_PatternDHT = 5;
+          count = 0;
+        }
+        else if (count >= loopPatternDHT[2] && LastDHTState == 5){
+          state_PatternDHT = 6;
+          count = 0;
+        }
+        else if (count >= loopPatternDHT[3] && LastDHTState == 6){
+          state_PatternDHT = 9;
+          count = 0;
+        }
+      break;
+      case(9): //DONE
+          if (inputPattern == -1) {
+            state_PatternDHT = 2;
+            count = 0;
+          }
+          else {
+            state_PatternDHT = 0; 
+          }
+      break;
+    }
+}
+
+void updateDelayTimer(){
+  inputDelayTime = 5000;
+  inputDelayTime = inputDelayTime*(log(abs(temperatureValue+1))/(temperatureValue+1)+1-log(abs(humidityValue/100+1)));
+  if (inputDelayTime > 10000) {
+    inputDelayTime = 10000;
+  }
 }
 
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
@@ -466,7 +711,16 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   inputPattern = inputData.pattern;
   inputIsActive = inputData.isActive;
   inputIsSync = inputData.isSync;
-  digitalWrite(toRegulatorA, HIGH);
+  state_PatternA = inputData.statePatternA;
+  state_PatternB = inputData.statePatternB;
+  state_PatternC = inputData.statePatternC;
+  state_PatternD = inputData.statePatternD;
+  if (DEBUG == true){
+    Serial.print("isActive: ");
+    Serial.println(inputIsActive);
+    Serial.print("isSync: ");
+    Serial.println(inputIsSync);
+  }
 }
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
